7264df71b656a6ef639b3435e73061071bcebaa3
ansarcodes <128506717+ansarcodes@users.noreply.github.com>
2026-04-11 06:50:14 +0500

    Add test books, rework logic

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-const myLibrary = [];
+const myLibrary = [
+    {
+        title: "Test1",
+        author: "Test1",
+        pages: 123,
+        read: "Read",
+        id: "373c1960-6735-41a8-bae8-53430eaca371"
+    },
+    {
+        title: "Test2",
+        author: "Test2",
+        pages: 321,
+        read: "Not read",
+        id: "38b4eaa5-6fc4-4dda-9222-4148b9b45bf0"
+    },
+    {
+        title: "Test3",
+        author: "Test3",
+        pages: 456,
+        read: "Not read",
+        id: "0a96f7aa-b790-4c52-8bbc-be8fbec92d78"
+    },
+    {
+        title: "Test4",
+        author: "Test4",
+        pages: 654,
+        read: "Read",
+        id: "3af124bd-12df-4a7e-8756-b88cd5db5bcd"
+    },
+];
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -14,7 +43,11 @@ function addBookToLibrary(title, author, pages, read) {
 }
 
 function displayBooks() {
+    const cardsContainer = document.querySelector("cards-container");
     myLibrary.forEach(book => {
-        
+        let newCard = document.createElement("div");
+        newCard.innerText = `${this.title}\n${this.author} ${this.pages}`;
+        newCard.setAttribute("class", "card");
+        newCard.setAttribute("id", this.id);
     })
 }
