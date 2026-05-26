1553059d745ae7596b6d5235a9b34f109d4043e0
ansarcodes <128506717+ansarcodes@users.noreply.github.com>
2026-05-26 14:16:17 +0500

    Refactor into using class

--- script.js
+++ script.js
@@ -29,12 +29,14 @@ const myLibrary = [
     },
 ];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.id = crypto.randomUUID();
+class Book{
+    constructor(title, author, pages, read){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.id = crypto.randomUUID();
+    }
 }
 
 function addBookToLibrary(title, author, pages, read) {
